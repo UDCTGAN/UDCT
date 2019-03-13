c13e9de2cf18a0563b3f56ff0f19f9c9d3836139
ihles <ihles@biomed.ee.ethz.ch>
2019-03-13 15:07:59 +0100

    Adding C Elegans script

---
+++ Scripts/train_UDCT_c_elegans.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Download the original BBBC dataset and extract it
+cd ../Data/C_Elegans/Original
+#wget https://data.broadinstitute.org/bbbc/BBBC010/BBBC010_v1_images.zip
+#unzip BBBC010_v1_images.zip -d ./
+#rm BBBC010_v1_images.zip
+#mv BBBC010_v1_images/* ./
+#rm -R BBBC010_v1_images
+
+# Transform the images to pngs
+cd ../../../notebooks/
+#jupyter nbconvert --to notebook --execute transform_c_elegans_tif_to_png.ipynb
+
+# Create the raw dataset
+echo "Creating the raw dataset"
+jupyter nbconvert --to notebook --execute make_raw_c_elegans.ipynb
+
+# Create the synthetic dataset
+echo "Creating the synthetic dataset"
+jupyter nbconvert --to notebook --execute make_synthetic_c_elegans.ipynb
+
+# Create the hdf5 file
+cd ../
+echo "Creating the hdf5 dataset"
+python create_h5_dataset.py ./Data/C_Elegans/Raw/ ./Data/C_Elegans/Synthetic/ ./Data/C_Elegans/c_elegans_dataset.h5
+
+# Train the network
+echo "Training the network"
+python main.py --dataset=./Data/C_Elegans/c_elegans_dataset.h5 --name=c_elegans_new
+
+# Create the generated synthetic images
+python main.py --dataset=./Data/C_Elegans/c_elegans_dataset.h5 --name=c_elegans_new --mode=gen_B 
